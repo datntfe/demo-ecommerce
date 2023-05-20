41a6e31d269965f01e169bbdca48ce84c4d7ca59
datnt-fe <datnt.fe@gmail.com>
2023-05-20 15:53:29 +0700

    add git

--- server.js
+++ server.js
@@ -33,6 +33,6 @@ app.prepare().then(() => {
     }
   }).listen(port, (err) => {
     if (err) throw err
-    console.log(`> Ready on https://localhost:${port}`)
+    console.log(`> Ready on http://localhost:${port}`)
   })
 })
